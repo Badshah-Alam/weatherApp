63dae8b09166131ac9da2bda1d288c93a432257c
Badshah Alam <134541829+Badshah-Alam@users.noreply.github.com>
2023-07-27 19:09:27 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Weather from './Components/Weather';
+import WeatherApp from './Components/WeatherApp';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+   
+    </div>
+  );
+}
+
+export default App;
